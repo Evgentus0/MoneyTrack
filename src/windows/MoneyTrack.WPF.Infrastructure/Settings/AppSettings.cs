88c5e1dbb4addfdfd064e8221125c13b9931c423
Eugene <zerom2016romanenko@gmail.com>
2021-07-14 12:21:36 +0300

    add setup project, update settings

--- src/windows/MoneyTrack.WPF.Infrastructure/Settings/AppSettings.cs
+++ src/windows/MoneyTrack.WPF.Infrastructure/Settings/AppSettings.cs
@@ -2,8 +2,23 @@
 {
     public class AppSettings
     {
-        public int NumberOfLastTransaction { get; set; }
-        public int TransactionPageSize { get; set; }
-        public string LiteDBConnection { get; set; }
+        public int NumberOfLastTransaction { get; set; } 
+        public int TransactionPageSize { get; set; } 
+        public string LiteDBConnection { get; set; } 
+
+        protected AppSettings()
+        {
+
+        }
+
+        public static AppSettings GetWithDefaultValues()
+        {
+            return new AppSettings
+            {
+                NumberOfLastTransaction = 15,
+                TransactionPageSize = 50,
+                LiteDBConnection = "Data.db"
+            };
+        }
     }
 }
