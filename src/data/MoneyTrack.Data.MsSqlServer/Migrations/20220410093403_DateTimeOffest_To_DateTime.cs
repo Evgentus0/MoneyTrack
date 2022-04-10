cdea7fe5ed574712abe7a3b679e94a95006cd9ae
Eugene <zerom2016romanenko@gmail.com>
2022-04-10 12:36:18 +0300

    Migration DateTimeOffset to DateTime

---
+++ src/data/MoneyTrack.Data.MsSqlServer/Migrations/20220410093403_DateTimeOffest_To_DateTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyTrack.Data.MsSqlServer.Migrations
+{
+    public partial class DateTimeOffest_To_DateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDttm",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "AddedDttm",
+                table: "Transactions",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
